d5bd011584b0455390b03ad00bd5d12676210d1e
Asekh <79318977+Asekh8463@users.noreply.github.com>
2021-06-02 20:07:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,403 @@
+questions = [
+    {
+        "question": "Governments should place more environmental restrictions on businesses and penalize businesses which pollute.",
+        "effect": {
+            "auth": 5,
+            "radc": -1,
+            "prog": 0,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Violent demonstrations are justified.",
+        "effect": {
+            "auth": 0,
+            "radc": 6,
+            "prog": 0,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "GMO crops should not be grown.",
+        "effect": {
+            "auth": 0,
+            "radc": 0,
+            "prog": -5,
+            "anth": -3,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "If action is taken quickly, climate change can be stopped.",
+        "effect": {
+            "auth": 1,
+            "radc": -2,
+            "prog": 2,
+            "anth": 1,
+            "nihi": -4,
+        }
+    },
+    {
+        "question": "We should live like our ancestors did.",
+        "effect": {
+            "auth": 0,
+            "radc": 5,
+            "prog": -8,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "A carbon tax is a good idea",
+        "effect": {
+            "auth": 4,
+            "radc": -2,
+            "prog": 0,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "We should reduce urbanization",
+        "effect": {
+            "auth": 2,
+            "radc": 1,
+            "prog": -4,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Induviduals should form organizations to help fight against climate change",
+        "effect": {
+            "auth": -5,
+            "radc": 0,
+            "prog": 0,
+            "anth": 0,
+            "nihi": -1
+        }
+    },
+    {
+        "question": "Renewable energy plants which hurt the local wildlife should still be built.",
+        "effect": {
+            "auth": 0,
+            "radc": 0,
+            "prog": 3,
+            "anth": 6,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Government involvement against climate change is bad.",
+        "effect": {
+            "auth": -8,
+            "radc": 0,
+            "prog": 0,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Climate change has gone too far to be prevented, and resources should instead be directed to handling its adverse affects.",
+        "effect": {
+            "auth": 1,
+            "radc": 1,
+            "prog": 2,
+            "anth": 2,
+            "nihi": 5
+        }
+    },
+    {
+        "question": "Investments in science and technology will help combat climate change.",
+        "effect": {
+            "auth": 0,
+            "radc": 0,
+            "prog": 6,
+            "anth": 0,
+            "nihi": -1
+        }
+    },
+    {
+        "question": "Rather than rapidly switching to renewables, we should gradually transition to prevent problems.",
+        "effect": {
+            "auth": 1,
+            "radc": -4,
+            "prog": -1,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "We should slow down technological progress.",
+        "effect": {
+            "auth": 0,
+            "radc": 0,
+            "prog": -4,
+            "anth": -1,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Endangered species should be saved even if it results in no direct benifit to humanity.",
+        "effect": {
+            "auth": 0,
+            "radc": 0,
+            "prog": 0,
+            "anth": -4,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Many people will die over the next century as a result of climate change.",
+        "effect": {
+            "auth": 0,
+            "radc": 0,
+            "prog": 0,
+            "anth": 0,
+            "nihi": 5
+        }
+    },
+    {
+        "question": "We should stop animal testing in labs.",
+        "effect": {
+            "auth": 0,
+            "radc": 0,
+            "prog": -2,
+            "anth": -3,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Governments should punish induviduals who excessively pollute.",
+        "effect": {
+            "auth": 6,
+            "radc": 0,
+            "prog": 0,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Rather than being directly involved, the government should delegate anti-climate change action to local organizations.",
+        "effect": {
+            "auth": -3,
+            "radc": -1,
+            "prog": 0,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "The present-day economic system cannot appropriately deal with the climate cricis, and radical change is needed.",
+        "effect": {
+            "auth": 0,
+            "radc": 6,
+            "prog": 1,
+            "anth": 0,
+            "nihi": 1
+        }
+    },
+    {
+        "question": "Individuals should boycott environmentally harmful organizations.",
+        "effect": {
+            "auth": -4,
+            "radc": -4,
+            "prog": 0,
+            "anth": 0,
+            "nihi": -1
+        }
+    },
+    {
+        "question": "Intensive farming should be banned.",
+        "effect": {
+            "auth": 3,
+            "radc": 2,
+            "prog": -3,
+            "anth": -5,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "It's best to elect politicians who will pass eco-friendly laws rather than attempt radical change.",
+        "effect": {
+            "auth": 1,
+            "radc": -6,
+            "prog": 0,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Direct action against polluting organizations and induviduals, even if violent, is justified.",
+        "effect": {
+            "auth": -2,
+            "radc": -6,
+            "prog": 0,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "It's more important to save human lives than to preserve biodiversity.",
+        "effect": {
+            "auth": 0,
+            "radc": 0,
+            "prog": 0,
+            "anth": 4,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "The birth rate should be reduced to prevent the strain on the planet's resources.",
+        "effect": {
+            "auth": 3,
+            "radc": 4,
+            "prog": 0,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "It's better to increase efficiency of fossil fuels than try quickly transitioning to renewable resources.",
+        "effect": {
+            "auth": 0,
+            "radc": -2,
+            "prog": 5,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Some industries, even though they release hugh levels of emissions, are too vital to be shut down.",
+        "effect": {
+            "auth": -2,
+            "radc": -2,
+            "prog": 3,
+            "anth": 1,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Organizations should pay for any damage they inflict on the environment.",
+        "effect": {
+            "auth": -1,
+            "radc": -2,
+            "prog": 0,
+            "anth": 0,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "We will have to accept a decrease in our standard of living to combat climate change.",
+        "effect": {
+            "auth": -1,
+            "radc": 0,
+            "prog": -2,
+            "anth": -1,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "There will be a refugee crisis due to climate change in this century, and we should preapre for it.",
+        "effect": {
+            "auth": 0,
+            "radc": 0,
+            "prog": 1,
+            "anth": 1,
+            "nihi": 2
+        }
+    },
+    {
+        "question": "Even if some lives are lost, eco-terrorism is acceptable if emissions are reduced as a result.",
+        "effect": {
+            "auth": -3,
+            "radc": 5,
+            "prog": -1,
+            "anth": -1,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "The international community should co-operate to reduce climate change.",
+        "effect": {
+            "auth": 1,
+            "radc": -2,
+            "prog": 1,
+            "anth": 0,
+            "nihi": -1
+        }
+    },
+ {
+        "question": "Efforts should be made to protect animals from climate change.",
+        "effect": {
+            "auth": 0,
+            "radc": 0,
+            "prog": 0,
+            "anth": -5,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "With appropriate action, the effects of climate change can be mostly reversed.",
+        "effect": {
+            "auth": 1,
+            "radc": 1,
+            "prog": 2,
+            "anth": 0,
+            "nihi": -4
+        }
+    },
+ {
+        "question": "Nuclear Plants are an acceptable alternative to conventional fossil fuels.",
+        "effect": {
+            "auth": 0,
+            "radc": 1,
+            "prog": 2,
+            "anth": 3,
+            "nihi": 0
+        }
+    },
+
+    {
+        "question": "No major change can occur on the level of induvidual actions.",
+        "effect": {
+            "auth": 0,
+            "radc": 1,
+            "prog": 2,
+            "anth": 3,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Humans are more important than animals.",
+        "effect": {
+            "auth": 0,
+            "radc": 0,
+            "prog": 0,
+            "anth": 2,
+            "nihi": 0
+        }
+    },
+    {
+        "question": "Organizations such as Greenpeace  are effective.",
+        "effect": {
+            "auth": 0,
+            "radc": 0,
+            "prog": 0,
+            "anth": 2,
+            "nihi": 0
+        }
+    }
+];
+
+
+
+
+
+
+
+
+
+
