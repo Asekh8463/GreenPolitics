e2dea787a48057541e099e1c6d8d2fd38c3a7eb5
Asekh <79318977+Asekh8463@users.noreply.github.com>
2021-06-11 13:20:29 +0530

    Create ideology.js

---
+++ ideology.js
@@ -0,0 +1,23 @@
+  
+ideologies = [
+    {
+        "name": "Eco-terrorism",
+        "stats": {
+            "auth": 75,
+            "radc": 100,
+            "prog": 25,
+            "anth": 25,
+            "nihi": 50,
+        }
+    },
+    {
+        "name": "Animal Liberationism",
+        "stats": {
+            "auth": 75,
+            "radc": 100,
+            "prog": 25,
+            "anth": 0,
+            "nihi": 50,
+        }
+    },
+  ];
